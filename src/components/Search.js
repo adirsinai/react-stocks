e9a874d3b85f45db09792362f5340fa93ad6f275
adirsinai <adirsinai@gmail.com>
2022-03-15 13:26:23 +0200

    start search build

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="form-content">
+      <form className="form-control">
+        <input type="text" className="searchTerm" placeholder="Add Stock..." />
+        <button className="cancel-btn">Cancel</button>
+      </form>
+    </div>
+  );
 };
 
 export default Search;
